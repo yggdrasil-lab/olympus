6c2d5874b177c0209d45f715411a158dd263dda5
Raymund <mon2x0@gmail.com>
2026-01-01 22:17:48 +1300

    recreate compose stack on deployment

--- start.sh
+++ start.sh
@@ -6,6 +6,9 @@ echo "Starting Olympus in Production mode..."
 # Define network name
 NETWORK_NAME="aether-net"
 
+# Ensure clean state
+docker compose -f docker-compose.yml -f docker-compose.prod.yml down --remove-orphans
+
 # Ensure network exists
 docker network inspect "$NETWORK_NAME" >/dev/null 2>&1 || docker network create --driver overlay --attachable "$NETWORK_NAME"
 
