86f156485490f4b9bedd065f594253e5a356fbe4
Raymund <mon2x0@gmail.com>
2026-01-02 21:23:00 +1300

    chore: Add `--skip-build` flag to deploy script calls in start scripts.

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 # Usage: ./scripts/deploy.sh <STACK_NAME> [COMPOSE_FILES...]
-./scripts/deploy.sh "olympus" docker-compose.yml docker-compose.prod.yml
+./scripts/deploy.sh --skip-build "olympus" docker-compose.yml docker-compose.prod.yml
--- start_dev.sh
+++ start_dev.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 source ./scripts/load_env.sh
-./scripts/deploy.sh "olympus_dev" docker-compose.yml docker-compose.dev.yml
+./scripts/deploy.sh --skip-build "olympus_dev" docker-compose.yml docker-compose.dev.yml
